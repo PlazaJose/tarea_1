92692a5cad5f001a8633d0942d56582090773ac2
PlazaJose <plaza.jose@correounivalle.edu.co>
2025-04-02 09:50:14 -0500

    Added Class save button added

--- app/src/main/java/com/example/tarea_1/Formulario_edicion.java
+++ app/src/main/java/com/example/tarea_1/Formulario_edicion.java
@@ -1,14 +1,25 @@
 package com.example.tarea_1;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.tarea_1.model.Informacion_academica;
+import com.example.tarea_1.model.Preferencias;
+import com.example.tarea_1.model.Usuario;
+
+import java.util.ArrayList;
 
 public class Formulario_edicion extends AppCompatActivity {
 
@@ -34,6 +45,26 @@ public class Formulario_edicion extends AppCompatActivity {
             "Celtic", "Gothic Rock", "New Wave", "Celtic Rock",
             "Kizomba",        // Estilo musical originario de Angola, mezcla de semba y música latina con un ritmo lento y sensual.
     };
+    EditText edt_nombre;
+    EditText edt_apellido;
+    EditText edt_edad;
+    EditText edt_correo;
+    EditText edt_telefono;
+    EditText edt_direccion;
+    EditText edt_identificacion;
+    RadioButton rb_sexo_hombre;
+    RadioButton rb_sexo_mujer;
+    RadioButton rb_sexo_otro;
+    RadioButton rb_sexo_nod;
+    EditText edt_institucion;
+    EditText edt_carrera;
+    EditText edt_ano_inicio;
+    EditText edt_ano_fin;
+    EditText edt_grado;
+    CheckBox cb_h_peliculas;
+    CheckBox cb_h_videojuegos;
+    CheckBox cb_h_deportes;
+    CheckBox cb_h_baile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,5 +90,66 @@ public class Formulario_edicion extends AppCompatActivity {
         spn_generos_m2.setAdapter(ad_generos_musicales);
         spn_generos_m3 = findViewById(R.id.spn_generos_m3);
         spn_generos_m3.setAdapter(ad_generos_musicales);
+
+        edt_nombre = findViewById(R.id.edt_nombre);
+        edt_apellido = findViewById(R.id.edt_apellido);
+        edt_edad = findViewById(R.id.edt_edad);
+        edt_correo = findViewById(R.id.edt_email);
+        edt_telefono = findViewById(R.id.edt_telefono);
+        edt_direccion = findViewById(R.id.edt_direccion);
+        edt_identificacion = findViewById(R.id.edt_identificacion);
+        rb_sexo_hombre = findViewById(R.id.rb_hombre);
+        rb_sexo_mujer = findViewById(R.id.rb_mujer);
+        rb_sexo_otro = findViewById(R.id.rb_otro);
+        rb_sexo_nod = findViewById(R.id.rb_no_decir);
+        edt_institucion = findViewById(R.id.edt_institucion);
+        edt_carrera = findViewById(R.id.edt_carrera);
+        edt_ano_inicio = findViewById(R.id.edt_ano_inicio);
+        edt_ano_fin = findViewById(R.id.edt_ano_fin);
+        edt_grado = findViewById(R.id.edt_grado);
+        cb_h_peliculas = findViewById(R.id.cb_peliculas);
+        cb_h_videojuegos = findViewById(R.id.cb_videojuegos);
+        cb_h_deportes = findViewById(R.id.cb_deportes);
+        cb_h_baile = findViewById(R.id.cb_baile);
+    }
+
+    public void guardar(View v){
+        String sexo = "prefiero no decir";
+        if(rb_sexo_hombre.isChecked())sexo = "Hombre";
+        if(rb_sexo_mujer.isChecked())sexo = "Mujer";
+        if(rb_sexo_otro.isChecked())sexo = "Otro";
+        Usuario usuario = new Usuario(
+                edt_nombre.getText().toString(),
+                edt_apellido.getText().toString(),
+                Integer.parseInt(edt_edad.getText().toString()),
+                edt_correo.getText().toString(),
+                edt_telefono.getText().toString(),
+                edt_direccion.getText().toString(),
+                sexo,
+                spn_tipo_doc.getSelectedItem().toString()+edt_identificacion.getText().toString(),
+                spn_estado_civil.getSelectedItem().toString()
+        );
+        Informacion_academica informacion_academica = new Informacion_academica(
+                edt_institucion.getText().toString(),
+                edt_carrera.getText().toString(),
+                Integer.parseInt(edt_ano_inicio.getText().toString()),
+                Integer.parseInt(edt_ano_fin.getText().toString()),
+                edt_grado.getText().toString()
+        );
+        usuario.setInformacion_academica(informacion_academica);
+        ArrayList<String> al_hobbies = new ArrayList<String>();
+        if(cb_h_peliculas.isChecked())al_hobbies.add("PELICULAS");
+        if(cb_h_videojuegos.isChecked())al_hobbies.add("VIDEOJUEGos");
+        if(cb_h_deportes.isChecked())al_hobbies.add("DEPORTES");
+        if(cb_h_baile.isChecked())al_hobbies.add("BAILE");
+        String[] hobbies = (al_hobbies.toArray(new String[0]));
+        String[] generos_musicales= {
+                spn_generos_m1.getSelectedItem().toString(),
+                spn_generos_m2.getSelectedItem().toString(),
+                spn_generos_m3.getSelectedItem().toString()
+        };
+        Preferencias preferencias = new Preferencias(hobbies, generos_musicales);
+        usuario.setPreferencias(preferencias);
+        Toast.makeText(this, usuario.toString(), Toast.LENGTH_SHORT).show();
     }
 }
