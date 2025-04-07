7845bbbe1a61f04dd4d4e71b66cfae6b257e5b1b
PlazaJose <plaza.jose@correounivalle.edu.co>
2025-04-07 18:37:40 -0500

    Added the opration

--- app/src/main/java/com/example/tarea_1/Calculadora.java
+++ app/src/main/java/com/example/tarea_1/Calculadora.java
@@ -12,11 +12,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.tarea_1.model.TheOperation;
+
 import java.util.ArrayList;
 
 public class Calculadora extends AppCompatActivity {
 
     ArrayList<String> operation_list = new ArrayList<>();
+    TheOperation theOperation;
     String current_info = "";
 
     String tier_list[] = {"(", "raiz", "*", "/", "+", "-"};
@@ -80,9 +83,9 @@ public class Calculadora extends AppCompatActivity {
     }
 
     public void C(View v){
-        if(current_info.length()>0){
+        if(!current_info.isEmpty()){
             current_info = "";
-        }else if (operation_list.size()>0) {
+        }else if (!operation_list.isEmpty()) {
             operation_list.remove(operation_list.size()-1);
             current_info = cplus();
         }
@@ -90,7 +93,7 @@ public class Calculadora extends AppCompatActivity {
     }
 
     private String cplus(){
-        if(operation_list.size()<1)return "";
+        if(operation_list.isEmpty())return "";
         String ci = operation_list.get(operation_list.size()-1);
         boolean is_op = false;
         for(int i = 0; i< tier_list.length; i++){
@@ -106,7 +109,7 @@ public class Calculadora extends AppCompatActivity {
     }
 
     public void delete(View v){
-        if(current_info.length()>0){
+        if(!current_info.isEmpty()){
             current_info = current_info.substring(0, current_info.length()-1);
         } else {
             C(v);
@@ -139,6 +142,10 @@ public class Calculadora extends AppCompatActivity {
             current_info += add_dot();
         }else if (v.getId() == R.id.bt_sign){
             current_info = change_sign();
+        }else if (v.getId() == R.id.bt_potencia){
+            current_info = add_potencia(current_info);
+        }else if (v.getId() == R.id.bt_raiz){
+            current_info = add_raiz(current_info);
         }
         show_operation();
     }
@@ -161,6 +168,22 @@ public class Calculadora extends AppCompatActivity {
         return ".";
     }
 
+    private String add_potencia(String n){
+        if(current_info == null) return "";
+        if(current_info.isEmpty()) return "";
+        if(current_info.contains(TheOperation.SIGNO_POTENCIA)) return n;
+        if(current_info.contains(TheOperation.SIGNO_RAIZ)) return n.replaceAll("[^0-9.-]", "");
+        return n+TheOperation.SIGNO_POTENCIA;
+    }
+    private String add_raiz(String n){
+        if(current_info == null) return "";
+        if(current_info.isEmpty()) return "";
+        if(current_info.contains(TheOperation.SIGNO_RAIZ)) return n;
+        if(current_info.contains("-")) return n;
+        if(current_info.contains(TheOperation.SIGNO_POTENCIA)) return n.replaceAll("[^0-9.-]", "");
+        return TheOperation.SIGNO_RAIZ+n;
+    }
+
     private String change_sign(){
         if(current_info == null) return current_info;
         if(current_info.isEmpty()) return current_info;
@@ -171,100 +194,10 @@ public class Calculadora extends AppCompatActivity {
 
     public void equal(View v){
         add_operation("");
-        current_info = operate_subsequence_r(operation_list).get(0);
+        theOperation = new TheOperation(operation_list);
+        current_info = theOperation.operate_subsequence_r(operation_list).get(0);//operate_subsequence_r(operation_list).get(0);
         operation_list = new ArrayList<>();
         show_operation();
-    }
-
-    ArrayList<String> operate_subsequence_r(ArrayList<String> ss){
-        ArrayList<String> result_tier_3 = new ArrayList<>();
-        boolean entered = false;
-        int omitir = -1;
-        for (int i = 0; i < ss.size(); i++) {
-            if (ss.get(i).equals("*") && !entered) {
-                if (i + 1 < ss.size()) {
-                    Double no1 = Double.valueOf(ss.get(i - 1));
-                    Double no2 = Double.valueOf(ss.get(i + 1));
-                    double r = no1 * no2;
-                    result_tier_3.set(result_tier_3.size()-1, "" + r);
-                    entered = true;
-                    omitir = i+1;
-                }
-            }else{
-                if(i!=omitir)result_tier_3.add(ss.get(i));
-            }
-        }
-        if(!check_tier(2, result_tier_3))return operate_subsequence_r(result_tier_3);
-        ArrayList<String> result_tier_4 = new ArrayList<>();
-        entered = false;
-        omitir = -1;
-        for (int i = 0; i < result_tier_3.size(); i++) {
-            if (result_tier_3.get(i).equals("/") && !entered) {
-                if (i + 1 < result_tier_3.size()) {
-
-                    Double no1 = Double.valueOf(result_tier_3.get(i - 1));
-                    Double no2 = Double.valueOf(result_tier_3.get(i + 1));
-                    double r = no1 / no2;
-                    result_tier_4.set(result_tier_4.size()-1, "" + r);
-                    entered = true;
-                    omitir = i+1;
-                    Toast.makeText(this, no1+":"+no2+":"+r, Toast.LENGTH_SHORT).show();
-                }
-            }else{
-                if(i!=omitir)result_tier_4.add(result_tier_3.get(i));
-            }
-        }
-        if(!check_tier(3, result_tier_4))return operate_subsequence_r(result_tier_4);
-        ArrayList<String> result_tier_5 = new ArrayList<>();   //nueva tier
-        entered = false;
-        omitir = -1;
-        for (int i = 0; i < result_tier_4.size(); i++) {      //vieja tier
-            if (result_tier_4.get(i).equals("+") && !entered) {//vieja tier validar operacion
-                if (i + 1 < result_tier_4.size()) {//vieja tier
-                    Double no1 = Double.valueOf(result_tier_4.get(i - 1));//vieja tier
-                    Double no2 = Double.valueOf(result_tier_4.get(i + 1));//vieja tier
-                    double r = no1 + no2; //operacion
-                    result_tier_5.set(result_tier_5.size()-1, "" + r);//nueva tier
-                    entered = true;
-                    omitir = i+1;
-                    Toast.makeText(this, no1+":"+no2+":"+r, Toast.LENGTH_SHORT).show();
-                }
-            }else{
-                if(i!=omitir)result_tier_5.add(result_tier_4.get(i));//nueva   (   vieja tier
-            }
-        }
-        if(!check_tier(4, result_tier_5))return operate_subsequence_r(result_tier_5);//nueva tier
-
-        ArrayList<String> result_tier_6 = new ArrayList<>();   //nueva tier
-        entered = false;
-        omitir = -1;
-        for (int i = 0; i < result_tier_5.size(); i++) {      //vieja tier
-            if (result_tier_5.get(i).equals("-") && !entered) {//vieja tier validar operacion
-                if (i + 1 < result_tier_5.size()) {//vieja tier
-                    Double no1 = Double.valueOf(result_tier_5.get(i - 1));//vieja tier
-                    Double no2 = Double.valueOf(result_tier_5.get(i + 1));//vieja tier
-                    double r = no1 - no2; //operacion
-                    result_tier_6.set(result_tier_6.size()-1, "" + r);//nueva tier
-                    entered = true;
-                    omitir = i+1;
-                    Toast.makeText(this, no1+":"+no2+":"+r, Toast.LENGTH_SHORT).show();
-                }
-            }else{
-                if(i!=omitir)result_tier_6.add(result_tier_5.get(i));//nueva   (   vieja tier
-            }
-        }
-        if(!check_tier(5, result_tier_6))return operate_subsequence_r(result_tier_6);//nueva tier
-        return result_tier_6;//nueva tier
-    }
-
-    private boolean check_tier(int tier, ArrayList<String> ss){
-        //String tier_list[] = {"(", "raiz", "*", "/", "+", "-"};
-        for (int i = 0; i < ss.size(); i++){
-            if(ss.get(i).equals(tier_list[tier])){
-                return false;
-            }
-        }
-        return true;
     }
 
 }
---
+++ app/src/main/java/com/example/tarea_1/model/TheOperation.java
@@ -0,0 +1,166 @@
+package com.example.tarea_1.model;
+
+import java.util.ArrayList;
+
+public class TheOperation {
+    ArrayList<String> operation_list;
+
+    public static String SIGNO_RAIZ = "√";
+    public static String SIGNO_POTENCIA = "²";
+    String[] tier_list = {"(", SIGNO_POTENCIA, SIGNO_RAIZ, "*", "/", "+", "-"};
+
+    public TheOperation(ArrayList<String> operation_list){
+        this.operation_list = operation_list;
+    }
+
+    public ArrayList<String> operate_subsequence_r(ArrayList<String> ss){
+        ArrayList<String> result_tier_1 = get_squeres(ss);
+        if(check_tier(1, result_tier_1))return operate_subsequence_r(result_tier_1);
+        ArrayList<String> result_tier_2 = get_squer_rooth(result_tier_1);
+        if(check_tier(2, result_tier_2))return operate_subsequence_r(result_tier_2);
+        ArrayList<String> result_tier_3 = get_multiplication(result_tier_2);
+        if(check_tier(3, result_tier_3))return operate_subsequence_r(result_tier_3);
+        ArrayList<String> result_tier_4 = get_division(result_tier_3);
+        if(check_tier(4, result_tier_4))return operate_subsequence_r(result_tier_4);
+        ArrayList<String> result_tier_5 = get_sums(result_tier_4);   //nueva tier
+        if(check_tier(5, result_tier_5))return operate_subsequence_r(result_tier_5);//nueva tier
+        ArrayList<String> result_tier_6 = get_restas(result_tier_5);   //nueva tier
+        if(check_tier(6, result_tier_6))return operate_subsequence_r(result_tier_6);//nueva tier
+        return result_tier_6;//nueva tier
+    }
+
+    private boolean check_tier(int tier, ArrayList<String> ss){
+        for (int i = 0; i < ss.size(); i++){
+            if(ss.get(i).contains(tier_list[tier])){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private ArrayList<String> get_squer_rooth(ArrayList<String> lista){
+        ArrayList<String> result_tier_2 = new ArrayList<>();
+        boolean entered = false;
+        //int omitir = -1;
+        for (int i = 0; i < lista.size(); i++) {
+            if (lista.get(i).contains(SIGNO_RAIZ) && !entered) {
+                entered = true;
+                String sn = lista.get(i).replaceAll("[^0-9.-]", "");
+                double number = Double.parseDouble(sn);
+                if(number<0)number = number*-1;
+                double r = Math.sqrt(number);
+                String rs = (Double.parseDouble(sn)<0?"ERROR":""+r);
+                result_tier_2.add(rs);
+            }else{
+                result_tier_2.add(lista.get(i));
+            }
+        }
+        return result_tier_2;
+    }
+    private ArrayList<String> get_squeres(ArrayList<String> lista){
+        ArrayList<String> result_tier_2 = new ArrayList<>();
+        boolean entered = false;
+        //int omitir = -1;
+        for (int i = 0; i < lista.size(); i++) {
+            if (lista.get(i).contains(SIGNO_POTENCIA) && !entered) {
+                entered = true;
+                String sn = lista.get(i).replaceAll("[^0-9.-]", "");
+                double number = Double.parseDouble(sn);
+                double r = number*number;
+                result_tier_2.add(""+r);
+            }else{
+                result_tier_2.add(lista.get(i));
+            }
+        }
+        return result_tier_2;
+    }
+
+    private ArrayList<String> get_multiplication(ArrayList<String> ss){
+        ArrayList<String> result_tier_3 = new ArrayList<>();
+        boolean entered = false;
+        int omitir = -1;
+        for (int i = 0; i < ss.size(); i++) {
+            if (ss.get(i).equals("*") && !entered) {
+                if (i + 1 < ss.size()) {
+                    Double no1 = Double.valueOf(ss.get(i - 1));
+                    Double no2 = Double.valueOf(ss.get(i + 1));
+                    double r = no1 * no2;
+                    result_tier_3.set(result_tier_3.size()-1, "" + r);
+                    entered = true;
+                    omitir = i+1;
+                }
+            }else{
+                if(i!=omitir)result_tier_3.add(ss.get(i));
+            }
+        }
+        return result_tier_3;
+    }
+
+    private ArrayList<String> get_division(ArrayList<String> result_tier_3){
+        ArrayList<String> result_tier_4 = new ArrayList<>();
+        boolean entered = false;
+        int omitir = -1;
+        for (int i = 0; i < result_tier_3.size(); i++) {
+            if (result_tier_3.get(i).equals("/") && !entered) {
+                if (i + 1 < result_tier_3.size()) {
+
+                    Double no1 = Double.valueOf(result_tier_3.get(i - 1));
+                    Double no2 = Double.valueOf(result_tier_3.get(i + 1));
+                    double r = no1 / no2;
+                    result_tier_4.set(result_tier_4.size()-1, "" + r);
+                    entered = true;
+                    omitir = i+1;
+                    //Toast.makeText(this, no1+":"+no2+":"+r, Toast.LENGTH_SHORT).show();
+                }
+            }else{
+                if(i!=omitir)result_tier_4.add(result_tier_3.get(i));
+            }
+        }
+        return result_tier_4;
+    }
+
+    private ArrayList<String> get_sums(ArrayList<String> result_tier_4){
+        ArrayList<String> result_tier_5 = new ArrayList<>();
+        boolean entered = false;
+        int omitir = -1;
+        for (int i = 0; i < result_tier_4.size(); i++) {      //vieja tier
+            if (result_tier_4.get(i).equals("+") && !entered) {//vieja tier validar operacion
+                if (i + 1 < result_tier_4.size()) {//vieja tier
+                    Double no1 = Double.valueOf(result_tier_4.get(i - 1));//vieja tier
+                    Double no2 = Double.valueOf(result_tier_4.get(i + 1));//vieja tier
+                    double r = no1 + no2; //operacion
+                    result_tier_5.set(result_tier_5.size()-1, "" + r);//nueva tier
+                    entered = true;
+                    omitir = i+1;
+                    //Toast.makeText(this, no1+":"+no2+":"+r, Toast.LENGTH_SHORT).show();
+                }
+            }else{
+                if(i!=omitir)result_tier_5.add(result_tier_4.get(i));//nueva   (   vieja tier
+            }
+        }
+        return result_tier_5;
+    }
+
+    private ArrayList<String> get_restas(ArrayList<String> result_tier_5){
+        ArrayList<String> result_tier_6 = new ArrayList<>();
+        boolean entered = false;
+        int omitir = -1;
+        for (int i = 0; i < result_tier_5.size(); i++) {      //vieja tier
+            if (result_tier_5.get(i).equals("-") && !entered) {//vieja tier validar operacion
+                if (i + 1 < result_tier_5.size()) {//vieja tier
+                    Double no1 = Double.valueOf(result_tier_5.get(i - 1));//vieja tier
+                    Double no2 = Double.valueOf(result_tier_5.get(i + 1));//vieja tier
+                    double r = no1 - no2; //operacion
+                    result_tier_6.set(result_tier_6.size()-1, "" + r);//nueva tier
+                    entered = true;
+                    omitir = i+1;
+                    //Toast.makeText(this, no1+":"+no2+":"+r, Toast.LENGTH_SHORT).show();
+                }
+            }else{
+                if(i!=omitir)result_tier_6.add(result_tier_5.get(i));//nueva   (   vieja tier
+            }
+        }
+        return result_tier_6;
+    }
+
+}
